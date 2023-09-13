b9aec3347d4751ae39fb5afa8aebf4bc3b2d691e
zanol <willian.zanol@totvs.com.br>
2023-09-13 17:33:32 -0300

    Initial

--- main.py
+++ main.py
@@ -10,12 +10,38 @@ from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAct
 
 logger = logging.getLogger(__name__)
 
-class FLogToolsExtension(Extension):
+class FLOGToolsExtension(Extension):
 
     def __init__(self):
         logger.info('init Flog Tools')
-        super(FLogToolsExtension, self).__init__()
+        super(FLOGToolsExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+    def generate_key():
+        current_date = datetime.today().strftime('%Y-%m-%d')
+        current_date_bytes = current_date.encode("ascii")
+
+        base64_bytes = base64.b64encode(current_date_bytes)
+        base64_string = base64_bytes.decode("ascii")
+
+        return FLOGToolsExtension.createItem("Tools Key", base64_string, CopyToClipboardAction(base64_string))
+
+    def create_suggestions():
+        suggestions = []
+
+        suggestions.append(FLOGToolsExtension.createItem(
+            "Sugestão: Tools Key",
+            "Comando para gerar key tools Financeiro",
+            CopyToClipboardAction("flog key")))
+
+        return suggestions
+
+    def createItem(name, description, on_enter):
+        return ExtensionResultItem(icon='images/icon.ico',
+                                  name=name,
+                                  description=description,
+                                  highlightable=False,
+                                  on_enter=on_enter)
 
 class KeywordQueryEventListener(EventListener):
 
@@ -25,22 +51,11 @@ class KeywordQueryEventListener(EventListener):
         query = event.get_argument() or str()
 
         if query == "key":
-            items.append(generate_key())
+            items.append(FLOGToolsExtension.generate_key())
+
+        items.extend(FLOGToolsExtension.create_suggestions())
 
         return RenderResultListAction(items)
 
-    def generate_key():
-        current_date = datetime.today().strftime('%Y-%m-%d')
-        current_date_bytes = current_date.encode("ascii")
-
-        base64_bytes = base64.b64encode(current_date_bytes)
-        base64_string = base64_bytes.decode("ascii")
-
-        return ExtensionResultItem(icon='images/icon.ico',
-                                    name="Tools Key",
-                                    description=base64_string,
-                                    highlightable=False,
-                                    on_enter=CopyToClipboardAction(base64_string))
-
 if __name__ == '__main__':
-    FLogToolsExtension().run()
+    FLOGToolsExtension().run()
