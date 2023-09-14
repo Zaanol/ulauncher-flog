52cce2294c02e7a91bd2cea9f22376df25b66638
zanol <willian.zanol@totvs.com.br>
2023-09-14 17:03:41 -0300

    Selenium

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from random import randint
+from time import sleep
+
 logger = logging.getLogger(__name__)
 
 class FLOGToolsExtension(Extension):
@@ -54,6 +61,29 @@ class KeywordQueryEventListener(EventListener):
             items.append(FLOGToolsExtension.generate_key())
 
         items.extend(FLOGToolsExtension.create_suggestions())
+
+        options = webdriver.ChromeOptions()
+        #options.headless = True
+
+        options.add_argument("--user-data-dir=/home/zanol/.config/google-chrome")
+        options.binary_location = "/opt/google/chrome/chrome"
+        options.add_argument("--profile-directory=Profile 1")
+        options.add_argument("--user-cache-dir=/home/zanol/.cache/google-chrome")
+        options.add_argument("--no-sandbox")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+
+        service = Service(r'/home/zanol/.local/share/ulauncher/extensions/com.github.zaanol.ulauncher-flog/chromedriver')
+
+        driver = webdriver.Chrome(service=service, options=options)
+        # Time to wait for element's presence
+        driver.implicitly_wait(10)
+        driver.get('https://login.tfc9924.k8s.us-central1.gcp.tks.sh/')
+        #print (driver.page_source.encode("utf-8"))
+
+        sleep(200)
+        driver.quit()
 
         return RenderResultListAction(items)
 
